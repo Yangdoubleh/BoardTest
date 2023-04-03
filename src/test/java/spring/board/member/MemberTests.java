d788d1c049ca826f3fe41d14c2173ecccd55de39
HyeonHo Yang <91450728+Yangdoubleh@users.noreply.github.com>
2023-04-04 00:11:44 +0900

    TDD를 통한 테스트 코드 구현

---
+++ src/test/java/spring/board/member/MemberTests.java
@@ -0,0 +1,37 @@
+package spring.board.member;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring.board.BoardApplicationTests;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * Member 등록 및 로그인 테스트
+ */
+@SpringBootTest
+@Transactional
+@Slf4j
+public class MemberTests extends BoardApplicationTests {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    /**
+     * 멤버 등록 테스트
+     */
+    @Test
+    public void memberInsert() {
+        Member member = new Member("Idtest", "Passwordtest", "Nicknametest");
+        Member insertMember = memberRepository.save(member);
+
+        log.info(insertMember.toString());
+
+        assertThat(insertMember.getId()).isEqualTo(member.getId());
+        assertThat(insertMember.getPassword()).isEqualTo(member.getPassword());
+        assertThat(insertMember.getNickname()).isEqualTo(member.getNickname());
+    }
+}
