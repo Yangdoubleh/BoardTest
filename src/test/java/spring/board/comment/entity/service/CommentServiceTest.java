93262fb9d2caded01308938fefc352c33ee46f6b
HyeonHo Yang <91450728+Yangdoubleh@users.noreply.github.com>
2023-04-30 21:52:03 +0900

    Feat(#2) : Add 댓글 등록 TestCode 작성

---
+++ src/test/java/spring/board/comment/entity/service/CommentServiceTest.java
@@ -0,0 +1,48 @@
+package spring.board.comment.entity.service;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring.board.board.entity.Board;
+import spring.board.board.repository.BoardRepository;
+import spring.board.comment.entity.Comment;
+import spring.board.comment.request.CommentReqeust;
+import spring.board.member.entity.Member;
+import spring.board.member.repository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CommentServiceTest {
+
+    @Autowired
+    BoardRepository boardRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    CommentService commentService;
+
+    @Test
+    @Transactional
+    void insertComment() {
+        Board board = boardRepository.findAll().stream()
+                .findFirst()
+                .get();
+
+        CommentReqeust commentReqeust = CommentReqeust.builder()
+                .boardseq(board.getBoardseq())
+                .contents("테스트댓글작성")
+                .build();
+
+        Member member = memberRepository.findAll().stream()
+                .findFirst()
+                .get();
+
+        Comment insertComment = commentService.insertComment(commentReqeust, member);
+
+        assertThat(insertComment.getBoard()).isEqualTo(board);
+        assertThat(insertComment.getContents()).isEqualTo(commentReqeust.getContents());
+        assertThat(insertComment.getMember()).isEqualTo(member);
+    }
+}
