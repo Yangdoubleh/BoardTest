80c1777791afe03acc580b13e6c65eff0ac91c26
HyeonHo Yang <91450728+Yangdoubleh@users.noreply.github.com>
2023-04-30 21:42:58 +0900

    Feat(#2) : Add 댓글 등록 Service 생성

---
+++ src/main/java/spring/board/comment/entity/service/CommentService.java
@@ -0,0 +1,61 @@
+package spring.board.comment.entity.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.board.board.entity.Board;
+import spring.board.board.repository.BoardRepository;
+import spring.board.comment.entity.Comment;
+import spring.board.comment.entity.repository.CommentRepository;
+import spring.board.comment.request.CommentReqeust;
+import spring.board.member.entity.Member;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@Transactional
+@Slf4j
+@AllArgsConstructor
+public class CommentService {
+
+    private final BoardRepository boardRepository;
+
+    private final CommentRepository commentRepository;
+
+    /**
+     * 댓글 등록
+     * @param commentReqeust
+     * @param member
+     * @return
+     */
+    @Transactional
+    public Comment insertComment(CommentReqeust commentReqeust, Member member) {
+        Comment insertComment = this.buildComment(commentReqeust, member);
+
+        return commentRepository.save(insertComment);
+    }
+
+    /**
+     * 댓글 객체 생성
+     * @param commentReqeust
+     * @param member
+     * @return
+     */
+    private Comment buildComment(CommentReqeust commentReqeust, Member member) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String now = localDateTime.format(dateTimeFormatter);
+
+        Board boardInfo = boardRepository.findOneByBoardseq(commentReqeust.getBoardseq());
+
+        return Comment.builder()
+                .commentseq(commentReqeust.getCommentseq())
+                .board(boardInfo)
+                .contents(commentReqeust.getContents())
+                .member(member)
+                .indate(now)
+                .build();
+    }
+}
